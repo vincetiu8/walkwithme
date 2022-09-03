d64fff9ae2461f46b8c490cb18bb3d8cb3f939cf
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2022-09-02 23:59:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, MemoryRouter, Routes} from "react-router";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<MemoryRouter>
+				<Routes>
+					<Route path="/">
+						{ /* Add home component here */}
+					</Route>
+					<Route path="/login">
+						{ /* Add login component here */}
+					</Route>
+				</Routes>
+			</MemoryRouter>
+		</div>
+	);
 }
 
 export default App;
